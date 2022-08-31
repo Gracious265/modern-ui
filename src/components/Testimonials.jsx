2e6aaa1a234af196efa042b9d606e7e34c2e7a95
Gracious Nguwo <graciousnguwo2020@gmail.com>
2022-08-31 15:02:57 +0200

    footer component done

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -14,7 +14,7 @@ const Testimonials = () =>
         <h1 className={styles.heading2}>What people are <br className='sm:block hidden'/>saying about us
         </h1>
         <div className='w-full md:mt-0 mt-6'>
-          <p className='text-left max-w-[450px]'>Everything you need to accept card payments and grow your business
+          <p className={`${styles.paragraph} text-left max-w-[450px]`}>Everything you need to accept card payments and grow your business
           anywhere on the planet.</p>
         </div>
       </div>
